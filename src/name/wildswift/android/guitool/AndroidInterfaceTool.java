f6631f87b20eddf9fa8ca45c668ce2fc86a30606
Alexander Karmanov <www.swift.ru@gmail.com>
2012-01-14 19:17:20 +0700

    first commit

---
+++ src/name/wildswift/android/guitool/AndroidInterfaceTool.java
@@ -0,0 +1,15 @@
+package name.wildswift.android.guitool;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidInterfaceTool extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
